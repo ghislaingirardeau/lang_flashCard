333218838dbd2085414f23fbae8342827b85f2d1
ghislaingirardeau <g.girardeaupro@sfr.fr>
2023-04-18 17:13:07 +0700

    feat: custom error page & get dynamic layout

--- app.vue
+++ app.vue
@@ -1,11 +1,17 @@
 <template>
   <div>
     <VitePwaManifest />
-    <NuxtLayout name="default">
+    <!-- DEFINE LAYOUT = if pass name, available for all page -->
+    <!-- OPT 1 <NuxtLayout name="default"> -->
+    <!-- OPT 2 <NuxtLayout :name="layoutName"> -->
+    <!-- OPT 3 if no name = it's default layout but can be change inside specific page-->
+    <NuxtLayout>
       <NuxtPage />
     </NuxtLayout>
   </div>
 </template>
+
+<script setup></script>
 
 <style lang="scss">
 body {
---
+++ error.vue
@@ -0,0 +1,11 @@
+<template>
+  <button @click="handleError">Back to home</button>
+  <h1>This page doesn't exist</h1>
+</template>
+
+<script setup>
+const props = defineProps({
+  error: Object,
+});
+const handleError = () => clearError({ redirect: "/" });
+</script>
