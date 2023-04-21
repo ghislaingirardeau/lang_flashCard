465ebb11b64507300a749dbadf45b89e87844932
ghislaingirardeau <g.girardeaupro@sfr.fr>
2023-04-21 16:23:03 +0700

    refactor: swiper on setup config

--- components/grid/MyRow.vue
+++ components/grid/MyRow.vue
@@ -56,7 +56,6 @@ export default {
         return breakpoints.smallerOrEqual("lg").value ? "95%" : "45%";
       }
     });
-    console.log(rowWidth.value);
     const onCardClick = (event) => {
       let elementWithId = useFindEltId(event.target);
       if (onRouteHome.value) {
--- components/grid/swiper.vue
+++ components/grid/swiper.vue
@@ -9,149 +9,136 @@
   </div>
 </template>
 
-<script>
-export default {
-  setup(props, { emit }) {
-    const scrollStartX = ref(0);
-    const scrollStartY = ref(0);
-    const scrollEndY = ref(0);
-    const cardsStore = useCardsStore();
-    const localePath = useLocalePath();
-    const route = useRoute();
+<script setup>
+const emit = defineEmits();
+const scrollStartX = ref(0);
+const scrollStartY = ref(0);
+const scrollEndY = ref(0);
+const cardsStore = useCardsStore();
+const localePath = useLocalePath();
+const route = useRoute();
 
-    const onRouteHome = computed(() => {
-      return route.params.id ? false : true;
-    });
+const onRouteHome = computed(() => {
+  return route.params.id ? false : true;
+});
 
-    const idCard = (id) => {
-      return route.params.id
-        ? cardsStore.cardItems[route.params.id].find(
-            (e) => e.id === parseInt(id)
-          )
-        : cardsStore.cards.find((e) => e.id === parseInt(id));
-    };
-
-    const doOnClickDelete = (param, id) => {
-      param
-        ? cardsStore.removeCard(param, id)
-        : cardsStore.removeItem(route.params.id, id);
-    };
-
-    const startDrag = (event) => {
-      scrollStartX.value = event.changedTouches[0].clientX;
-      scrollStartY.value = event.changedTouches[0].clientY;
-    };
-    const endDrag = (event) => {
-      const defineTouchX = event.changedTouches[0].clientX - scrollStartX.value;
-      const defineTouchY = event.changedTouches[0].clientY - scrollStartY.value;
-
-      // get id to find the elements
-      let elementWithId = useFindEltId(event.target);
-      // set scrollendY = so when swipe again, it start from where it ended
-      // stop counting when the scroll is over the main container
-      if (defineTouchX > -50 && defineTouchX < 50) {
-        let newScroll =
-          scrollEndY.value +
-          (event.changedTouches[0].clientY - scrollStartY.value);
-        if (newScroll < 0) {
-          scrollEndY.value = 0;
-        } else if (newScroll > useGetMainHeightScroll(onRouteHome.value)) {
-          scrollEndY.value = useGetMainHeightScroll(onRouteHome.value);
-        } else {
-          scrollEndY.value = newScroll;
-        }
-      }
-
-      // on touch TAP
-      if (defineTouchY === 0 && defineTouchX === 0) {
-        // get the first parent with an id
-        let cardDetails;
-
-        if (elementWithId.includes("swipe-")) {
-          // if id includes swipe = so it's a click on delete block
-          cardDetails = idCard(elementWithId.replace("swipe-", ""));
-          doOnClickDelete(cardDetails.title, cardDetails.id);
-          return;
-        }
-        if (elementWithId.includes("card-")) {
-          // if on home route
-          if (onRouteHome.value) {
-            cardDetails = idCard(elementWithId.replace("card-", ""));
-            return navigateTo(
-              localePath({
-                name: "card-id",
-                params: { id: cardDetails.title },
-              })
-            );
-          } else {
-            // if on id route
-            // common with row click
-            document
-              .getElementById(`swipe-${elementWithId.replace("card-", "")}`)
-              ?.classList.add("hide");
-            useTextToPlay(
-              event,
-              parseInt(elementWithId.replace("card-", "")),
-              emit
-            );
-            return;
-          }
-        }
-      }
-    };
-
-    const scrollElement = (event) => {
-      const defineTouchY = event.changedTouches[0].clientY - scrollStartY.value;
-      const defineTouchX = event.changedTouches[0].clientX - scrollStartX.value;
-      // if scrollY long enough detected
-
-      let elementWithId = useFindEltId(event.target);
-
-      // si tu scroll sur un coté : hide or unhide the delete option et tu arretes la fonction
-      if (defineTouchX > 50) {
-        useAnimDeleteIcon(
-          elementWithId.replace("card-", ""),
-          onRouteHome.value,
-          "add",
-          "remove"
-        );
-        return;
-      }
-      if (defineTouchX < -50) {
-        console.log("show");
-        useAnimDeleteIcon(
-          elementWithId.replace("card-", ""),
-          onRouteHome.value,
-          "remove",
-          "add"
-        );
-        return;
-      }
-
-      // scroll vertical
-      if (
-        (defineTouchY < -10 || defineTouchY > 10) &&
-        (defineTouchX > -50 || defineTouchX < 50)
-      ) {
-        document.querySelector(".el-main").scroll(
-          0,
-          scrollEndY.value +
-            (event.changedTouches[0].clientY - scrollStartY.value) // position it ended + dynamic scroll - depart position of the scroll
-        );
-      }
-    };
-
-    onMounted(() => {
-      useScrollTo("auto");
-    });
-
-    return {
-      startDrag,
-      endDrag,
-      scrollElement,
-    };
-  },
+const idCard = (id) => {
+  return route.params.id
+    ? cardsStore.cardItems[route.params.id].find((e) => e.id === parseInt(id))
+    : cardsStore.cards.find((e) => e.id === parseInt(id));
 };
+
+const doOnClickDelete = (param, id) => {
+  param
+    ? cardsStore.removeCard(param, id)
+    : cardsStore.removeItem(route.params.id, id);
+};
+
+const startDrag = (event) => {
+  scrollStartX.value = event.changedTouches[0].clientX;
+  scrollStartY.value = event.changedTouches[0].clientY;
+};
+const endDrag = (event) => {
+  const defineTouchX = event.changedTouches[0].clientX - scrollStartX.value;
+  const defineTouchY = event.changedTouches[0].clientY - scrollStartY.value;
+
+  // get id to find the elements
+  let elementWithId = useFindEltId(event.target);
+  // set scrollendY = so when swipe again, it start from where it ended
+  // stop counting when the scroll is over the main container
+  if (defineTouchX > -50 && defineTouchX < 50) {
+    let newScroll =
+      scrollEndY.value + (event.changedTouches[0].clientY - scrollStartY.value);
+    if (newScroll < 0) {
+      scrollEndY.value = 0;
+    } else if (newScroll > useGetMainHeightScroll(onRouteHome.value)) {
+      scrollEndY.value = useGetMainHeightScroll(onRouteHome.value);
+    } else {
+      scrollEndY.value = newScroll;
+    }
+  }
+
+  // on touch TAP
+  if (defineTouchY === 0 && defineTouchX === 0) {
+    // get the first parent with an id
+    let cardDetails;
+
+    if (elementWithId.includes("swipe-")) {
+      // if id includes swipe = so it's a click on delete block
+      cardDetails = idCard(elementWithId.replace("swipe-", ""));
+      doOnClickDelete(cardDetails.title, cardDetails.id);
+      return;
+    }
+    if (elementWithId.includes("card-")) {
+      // if on home route
+      if (onRouteHome.value) {
+        cardDetails = idCard(elementWithId.replace("card-", ""));
+        return navigateTo(
+          localePath({
+            name: "card-id",
+            params: { id: cardDetails.title },
+          })
+        );
+      } else {
+        // if on id route
+        // common with row click
+        document
+          .getElementById(`swipe-${elementWithId.replace("card-", "")}`)
+          ?.classList.add("hide");
+        useTextToPlay(
+          event,
+          parseInt(elementWithId.replace("card-", "")),
+          emit
+        );
+        return;
+      }
+    }
+  }
+};
+
+const scrollElement = (event) => {
+  const defineTouchY = event.changedTouches[0].clientY - scrollStartY.value;
+  const defineTouchX = event.changedTouches[0].clientX - scrollStartX.value;
+  // if scrollY long enough detected
+
+  let elementWithId = useFindEltId(event.target);
+
+  // si tu scroll sur un coté : hide or unhide the delete option et tu arretes la fonction
+  if (defineTouchX > 50) {
+    useAnimDeleteIcon(
+      elementWithId.replace("card-", ""),
+      onRouteHome.value,
+      "add",
+      "remove"
+    );
+    return;
+  }
+  if (defineTouchX < -50) {
+    console.log("show");
+    useAnimDeleteIcon(
+      elementWithId.replace("card-", ""),
+      onRouteHome.value,
+      "remove",
+      "add"
+    );
+    return;
+  }
+
+  // scroll vertical
+  if (
+    (defineTouchY < -10 || defineTouchY > 10) &&
+    (defineTouchX > -50 || defineTouchX < 50)
+  ) {
+    document.querySelector(".el-main").scroll(
+      0,
+      scrollEndY.value + (event.changedTouches[0].clientY - scrollStartY.value) // position it ended + dynamic scroll - depart position of the scroll
+    );
+  }
+};
+
+onMounted(() => {
+  useScrollTo("auto");
+});
 </script>
 
 <style lang="scss" scoped>
