bdd36a1e222f51b01534b51d27c435d077782d3c
ghislaingirardeau <g.girardeaupro@sfr.fr>
2023-06-02 12:34:42 +0700

    perf: lighthouse eliminate render blocking

--- nuxt.config.js
+++ nuxt.config.js
@@ -34,14 +34,14 @@ export default defineNuxtConfig({
       link: [
         { rel: "preconnect", href: "https://fonts.googleapis.com" },
         { rel: "preconnect", href: "https://fonts.gstatic.com" },
-        {
+        /* {
           href: "https://fonts.googleapis.com/css2?family=Alkatra&display=swap",
           rel: "stylesheet",
         },
         {
           href: "https://fonts.googleapis.com/css2?family=Delicious+Handrawn&display=swap",
           rel: "stylesheet",
-        },
+        }, */
       ],
     },
     pageTransition: { name: "page", mode: "out-in" },
